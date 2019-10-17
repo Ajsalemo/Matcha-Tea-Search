ca17aed8b6beb594bd8822d3f024cfbbd9987016
Anthony Salemo <ajssalemo@gmail.com>
2019-10-17 18:19:21 -0400

    created a apollo provider hoc and abstracted the form component out

--- matcha-tea-search/src/components/submitform.js
+++ matcha-tea-search/src/components/submitform.js
@@ -1,48 +1,47 @@
 // ----------------------------------- Imports -------------------------------------- //
 // ---------------------------------------------------------------------------------- //
 
-import { Grid, Typography } from '@material-ui/core';
+import { TextField, InputAdornment } from '@material-ui/core';
 import React from 'react';
 import styled from 'styled-components';
-import SubmitForm from './submitform';
+import { Search } from '@material-ui/icons';
 
 // ---------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------- //
 
-const HeroTypography = styled(Typography)`
-    color: #fff;
-    margin-top: 0.8em;
-    width: fit-content;
-    margin: 0 auto;
-    font-family: Josefin Sans, Arial, sans-serif;
-    background-color: #008000c2;
-    padding: 0.2em 0.3em 0em 0.3em;
-    text-align: center;
+const TextfieldInput = styled(TextField)`
+    width: 70%;
+    margin: 7em auto 0em auto;
+    background-color: #fff;
 `;
 
-const HeroGrid = styled(Grid)`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    padding-top: 3.5em;
+const SearchIcon = styled(Search)`
+    &:hover {
+        cursor: pointer;
+    }
 `;
 
 // ---------------------------------------------------------------------------------- //
 
-const Hero = () => {
+const SubmitForm = () => {
     return (
-        <HeroGrid item sm={12} md={10}>
-            <HeroTypography variant="h1">Matcha Tea</HeroTypography>
-            <HeroTypography variant="subtitle1">Search for local Matcha and Bubble tea</HeroTypography>
-            <SubmitForm />
-        </HeroGrid>
+        <TextfieldInput 
+            id="search-for-location"
+            label="Search for locations"
+            placeholder="Ex. Charlotte, NC"
+            margin="normal"
+            variant="filled"
+            InputProps={{
+                endAdornment: <InputAdornment position='end'><SearchIcon /></InputAdornment>
+            }}
+        />
     );
 }
 
 // ---------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------- //
 
-export default Hero;
+export default SubmitForm;
 
 // ---------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------- //
