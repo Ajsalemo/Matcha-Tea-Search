eb65513cc1b818c4542ea43a672fcfe9686ff1e3
Anthony Salemo <ajssalemo@gmail.com>
2019-10-30 16:22:20 -0400

    now able to successfully call yelp's api using cors-anywhere

--- matcha-tea-search/src/apollo/apolloclient.js
+++ matcha-tea-search/src/apollo/apolloclient.js
@@ -8,13 +8,14 @@ import fetch from 'isomorphic-fetch';
 // ---------------------------------------------------------------------------------- //
 
 export const client = new ApolloClient({
-  uri: 'https://api.yelp.com/v3/graphql',
+  uri: 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/graphql',
   fetch,
   request: operation => {
     operation.setContext(context => ({
       headers: {
         ...context.headers,
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${bearer_token}`,
+        'Accept-Language': 'en_US'
       }
     }))
   }
