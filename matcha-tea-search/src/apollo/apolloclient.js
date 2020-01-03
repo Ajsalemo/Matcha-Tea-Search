43dd01bdba1309f1782d0735cbc4992b34ea3583
Anthony Salemo <ajssalemo@gmail.com>
2020-01-03 18:52:20 -0500

    merging changes

--- matcha-tea-search/src/apollo/apolloclient.js
+++ matcha-tea-search/src/apollo/apolloclient.js
@@ -2,18 +2,19 @@
 // ---------------------------------------------------------------------------------- //
 
 import ApolloClient from 'apollo-boost';
- 
+import fetch from 'isomorphic-fetch';
+
 // ---------------------------------------------------------------------------------- //
 // ---------------------------------------------------------------------------------- //
 
 export const client = new ApolloClient({
-  uri: process.env.APOLLO_CLIENT_URI,
+  uri: '/graphql',
   fetch,
   request: operation => {
     operation.setContext(context => ({
       headers: {
         ...context.headers,
-        Authorization: process.env.YELP_API_KEY,
+        Authorization: 'Bearer OsETErBzpyetI6eZewoap7zlFpSP32576voj3-ZpZhb2ca7RbSm9Pkg0z_ymJe4coNIATmbI1LTBgaTExaVGX1M0W4EfmbovFQFbD3v5Ai506_Mg9r20h1gkOfK5XXYx',
         'Accept-Language': 'en_US'
       }
     }))
